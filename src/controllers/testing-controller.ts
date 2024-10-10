f2a3ed918dcb0db736380cfabb8b13588e25c243
seeyous00n <borovikov.learn@gmail.com>
2024-10-10 19:01:26 +0300

    refactor method deleteAllVideo

--- src/controllers/testing-controller.ts
+++ src/controllers/testing-controller.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { db } from '../db';
 import { STATUS_CODE } from '../settings';
+import { setDb } from '../db';
 
 class TestController {
   deleteAllVideo = (req: Request, res: Response, next: NextFunction) => {
     try {
-      db.videos = [];
-      res.status(STATUS_CODE.NO_CONTENT).json('All data is deleted');
+      setDb();
+      res.status(STATUS_CODE.NO_CONTENT_204).json('All data is deleted');
     } catch (e) {
-      res.status(STATUS_CODE.BAD_REQUEST).json('Ups..');
+      res.status(STATUS_CODE.BAD_REQUEST_400).json('Ups..');
     }
   };
 }
