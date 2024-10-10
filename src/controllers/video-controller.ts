4766680a6dc17082f95f3fc75a0c0beb0319e4b2
seeyous00n <borovikov.learn@gmail.com>
2024-10-10 19:02:55 +0300

    refactor video-controller

--- src/controllers/video-controller.ts
+++ src/controllers/video-controller.ts
@@ -9,23 +9,23 @@ import { STATUS_CODE } from '../settings';
 class VideoController {
   getVideos = (req: Request, res: Response, next: NextFunction) => {
     const result = videoService.getAllVideos();
-    res.status(STATUS_CODE.OK).json(result);
+    res.status(STATUS_CODE.OK_200).json(result);
   };
 
   getVideo = (req: Request, res: Response, next: NextFunction) => {
     try {
       if (!req.params.id) {
-        res.status(STATUS_CODE.NO_CONTENT).json('id doesn\'t exist');
+        res.status(STATUS_CODE.NO_CONTENT_204).json('id doesn\'t exist');
         return;
       }
       const video = videoService.getVideo(+req.params.id);
       if (!video) {
-        res.status(STATUS_CODE.NOT_FOUND).json('id doesn\'t exist');
+        res.status(STATUS_CODE.NOT_FOUND_404).json('id doesn\'t exist');
         return;
       }
-      res.status(STATUS_CODE.OK).json(video);
+      res.status(STATUS_CODE.OK_200).json(video);
     } catch (error) {
-      res.status(STATUS_CODE.SERVER_ERROR).json('Ups..');
+      res.status(STATUS_CODE.SERVER_ERROR_500).json('Ups..');
     }
   };
 
@@ -33,54 +33,54 @@ class VideoController {
     try {
       const errors = new ValidationVideoRequest(req.body);
       if (errors.errors.length > 0) {
-        res.status(STATUS_CODE.BAD_REQUEST).json({ errorsMessages: errors.errors });
+        res.status(STATUS_CODE.BAD_REQUEST_400).json({ errorsMessages: errors.errors });
         return;
       }
       const videoDto = new VideoDto(req.body);
       const newVideo = videoService.createVideo(videoDto);
-      res.status(STATUS_CODE.CREATED).json(newVideo);
+      res.status(STATUS_CODE.CREATED_201).json(newVideo);
     } catch (error) {
-      res.status(STATUS_CODE.SERVER_ERROR).json('Ups..');
+      res.status(STATUS_CODE.SERVER_ERROR_500).json('Ups..');
     }
   };
 
   updateVideo = (req: Request, res: Response, next: NextFunction) => {
     try {
       if (!req.params.id || !videoService.getVideo(+req.params.id)) {
-        res.status(STATUS_CODE.NOT_FOUND).json('Not Found');
+        res.status(STATUS_CODE.NOT_FOUND_404).json('Not Found');
         return;
       }
 
       const errors = new ValidationUpdateVideoRequest(req.body);
 
       if (errors.errors.length > 0) {
-        res.status(STATUS_CODE.BAD_REQUEST).json({ errorsMessages: errors.errors });
+        res.status(STATUS_CODE.BAD_REQUEST_400).json({ errorsMessages: errors.errors });
         return;
       }
-      
+
       const dataVideo = new UpdateVideDto(req.body);
       videoService.updateVideo(+req.params.id, dataVideo);
-      res.status(STATUS_CODE.NO_CONTENT).json();
+      res.status(STATUS_CODE.NO_CONTENT_204).json();
     } catch (error) {
-      res.status(STATUS_CODE.SERVER_ERROR).json('Ups..');
+      res.status(STATUS_CODE.SERVER_ERROR_500).json('Ups..');
     }
   };
 
   deleteVideo = (req: Request, res: Response, next: NextFunction) => {
     try {
       if (!req.params.id) {
-        res.status(STATUS_CODE.NOT_FOUND).json('Not Found');
+        res.status(STATUS_CODE.NOT_FOUND_404).json('Not Found');
         return;
       }
       const result = videoService.deleteVideo(+req.params.id);
 
       if (!result) {
-        res.status(STATUS_CODE.NOT_FOUND).json('Not Found');
+        res.status(STATUS_CODE.NOT_FOUND_404).json('Not Found');
         return;
       }
-      res.status(STATUS_CODE.NO_CONTENT).json();
+      res.status(STATUS_CODE.NO_CONTENT_204).json();
     } catch (e) {
-      res.status(STATUS_CODE.SERVER_ERROR).json('Ups..');
+      res.status(STATUS_CODE.SERVER_ERROR_500).json('Ups..');
     }
   };
 }
