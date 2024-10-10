a457a0c4b48bb4e874b093e302488ca1ca6565b4
seeyous00n <borovikov.learn@gmail.com>
2024-10-10 18:59:46 +0300

    create func for db

--- src/db.ts
+++ src/db.ts
@@ -3,3 +3,12 @@ import { IDb } from './types/video-types';
 export const db: IDb = {
   videos: [],
 };
+
+export const setDb = (dataset?: IDb) => {
+  if (!dataset) {
+    db.videos = [];
+    return;
+  }
+
+  db.videos = dataset.videos || db.videos;
+};
