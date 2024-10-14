119005a6a2dcf3e9e13f4ae44e5b9dee3ef6e1c8
seeyous00n <borovikov.learn@gmail.com>
2024-10-14 17:35:17 +0300

    refactor: rename and change method setDb

--- src/controllers/testing-controller.ts
+++ src/controllers/testing-controller.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { HTTP_MESSAGE, HTTP_STATUS_CODE } from '../settings';
-import { setDb } from '../db';
+import { clearDb } from '../db';
 
 class TestController {
   deleteAllVideo = (req: Request, res: Response, next: NextFunction) => {
     try {
-      setDb();
+      clearDb();
       res.status(HTTP_STATUS_CODE.NO_CONTENT_204).json();
     } catch (e) {
       res.status(HTTP_STATUS_CODE.BAD_REQUEST_400).json(HTTP_MESSAGE.BAD_REQUEST);
--- src/db.ts
+++ src/db.ts
@@ -4,11 +4,4 @@ export const db: IDb = {
   videos: [],
 };
 
-export const setDb = (dataset?: IDb) => {
-  if (!dataset) {
-    db.videos = [];
-    return;
-  }
-
-  db.videos = dataset.videos || db.videos;
-};
+export const clearDb = () => db.videos = [];
